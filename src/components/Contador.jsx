5e899d3c589a3fc555a07e8bacf7d08fb3f4094d
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-13 15:21:56 -0300

    agregamos botones al contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Contador = () => {
   return (
-    <section className='border rounded-3'>
-        <h2 className='Text-center'>Contador</h2>
-      
+    <section className='border rounded-3 text-center py-3'>
+        <h2 >Contador</h2>
+      <p className='lead '>1</p>
+      <button className='btn btn-primary me-3'>+</button>
+      <button className='btn btn-danger'>-</button>
     </section>
   ); 
 };
