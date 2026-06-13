a07964b637508332607fc35a624602628330f73a
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-13 15:51:39 -0300

    agregamos el evento onclick en el boton incremntar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const Contador = () => {
+    let numero = 0
+    const incrementar = () {
+        numero++
+    }
   return (
     <section className='border rounded-3 text-center py-3'>
         <h2 >Contador</h2>
       <p className='lead '>1</p>
-      <button className='btn btn-primary me-3'>+</button>
+      <button className='btn btn-primary me-3' onClick={incrementar}>+</button>
       <button className='btn btn-danger'>-</button>
     </section>
   ); 
