d25b78a9d04fab371b25348f971e0ed0775d7319
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-13 17:17:42 -0300

    incrementamos el numero usando state

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import { useState} from "react";
 
 const Contador = () => {
-    let numero = 0
-    const incrementar = () {
-        numero++
-    }
+    const [numero, setNumero] = useState(0)
+  //let numero = 0;
+  const incrementar = () => {
+    numero = numero + 1;
+    console.log(numero);
+  };
   return (
-    <section className='border rounded-3 text-center py-3'>
-        <h2 >Contador</h2>
-      <p className='lead '>1</p>
-      <button className='btn btn-primary me-3' onClick={incrementar}>+</button>
-      <button className='btn btn-danger'>-</button>
+    <section className="border rounded-3 text-center py-3">
+      <h2>Contador</h2>
+      <p className="lead ">{numero}</p>
+      <button className="btn btn-primary me-3" onClick={incrementar}>
+        +
+      </button>
+      <button className="btn btn-danger">-</button>
     </section>
-  ); 
+  );
 };
 
 export default Contador;
